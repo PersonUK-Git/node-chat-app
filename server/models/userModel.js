db8fede1704846aac2d66e16a19f3f0f97496bbd
ppprateek <ppprateeksharma006@gmail.com>
2023-04-14 19:52:22 +0530

    first commit

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: true,
+  },
+  avatarImage: {
+    type: String,
+    default: "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMc",
+  },
+});
+
+module.exports = mongoose.model("Users", userSchema);
